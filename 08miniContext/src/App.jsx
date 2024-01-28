d0c43a7ccd6ae79ce3ff323483be9a19530d271f
edson <edsondsouzadev@gmail.com>
2024-01-28 07:21:18 +0530

    context api mini login project

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <h1 className='text-3xl font-bold'>Mini Context</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
